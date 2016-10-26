8079c57d0c5f178e0b58dd2b041df901f6efdb55
Eddy Hu <guoning.hu@sas.com>
2016-10-26 09:53:57 +0800

    Refactor

---
+++ src/com/Operator.java
@@ -0,0 +1,46 @@
+package com;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String> oper = new Stack<String>();
+		Stack<Double> value = new Stack<Double>();
+		Scanner scan = new Scanner(System.in);
+		String ss = null;
+		ss = scan.next();
+		
+		char arr[] = ss.toCharArray();
+		for (char c : arr) {
+			String s = String.valueOf(c);
+			if (s.equals("(") || s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+				oper.push(s);
+				continue;
+			}else if (!s.equals(")")) {
+				value.push(Double.valueOf(s));
+				continue;
+			}else if (s.equals(")")) {
+				double a = value.pop();
+				double b = value.pop();
+				String op = oper.pop();
+				oper.pop();
+				if (op.equals("+")) {
+					b = a + b;
+				}else if (op.equals("-")) {
+					b = a - b;
+				}else if (op.equals("*")) {
+					b = a * b;
+				}else if (op.equals("/")) {
+					b = a / b;
+				}
+				value.push(Double.valueOf(b));
+			}
+		}
+		
+		System.out.println(value.pop());
+	}
+
+}
---
+++ src/com/ShellSort.java
@@ -0,0 +1,35 @@
+package com;
+
+public class ShellSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr [] = {2,4,5,2,9,4,8,3,9,2,0,1,12,30,25,26,27,10};
+		int len = arr.length;
+		int h = 1;
+		while (h < len) { //1, 4, 13, 40...
+			h = 3*h + 1;
+		}
+		
+		while (h >= 1) {
+			for (int i = h; i < len; i++) {		// The step should be the descending order to eliminate and 
+												// step length cannot exceed the array length
+				for (int j = i; j>=h && arr[j]<arr[j-h]; j-=h) {
+					// Swap the position if the previous is less then the latter one
+					int temp = arr[j];
+					arr[j] = arr[j-h];
+					arr[j-h] = temp;
+				}
+			}
+			
+			// Go to the next step length
+			h = h/3;
+			
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
