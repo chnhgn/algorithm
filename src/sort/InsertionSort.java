20f33d79605424d36a49ef187ffae2c89aedb5c5
Eddy Hu <guoning.hu@sas.com>
2016-10-26 17:33:18 +0800

    Adding insert sort

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -4,7 +4,20 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int arr [] = {2,4,5,7,9,10,8,3,6,1,2,7,9};
+		for (int i = 1; i < arr.length; i++) {
+			int j = i;
+			while (j>0 && arr[j]<arr[j-1]) {
+				int temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		for (int i : arr) {
+			System.out.println(i + " ");
+		}
 	}
 
 }
