f796cdc9af305b59ec841383c7c8af4423b28680
Eddy Hu <guoning.hu@sas.com>
2016-12-09 16:36:32 +0800

    Refactor the merge-sort algorithm

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,84 +1,59 @@
 package sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
-	public void merge(int arr[], int low, int high) {
-		// TODO Auto-generated method stub
-		int mid = (high-low)/2+low;
-		int prior = low;		// First half
-		int posterior = mid + 1;	// Second half
-		int buffer[] = new int[high-low+1];		// This is the buffer array to save the compare result
-		int k = 0;		// Buffer index
+	public static int[] sort(int[] nums, int low, int high) {  
+        int mid = (low + high) / 2; 
+        // Split the left/right part to smaller part then merge the atom part recursively
+        if (low < high) {  
+            // Left part 
+            sort(nums, low, mid);  
+            // Right part 
+            sort(nums, mid + 1, high);  
+            // Merge the left part and right part  
+            merge(nums, low, mid, high);  
+        }  
+        return nums;  
+    }
+	
+	
+	public static void merge(int[] nums, int low, int mid, int high) {
+		int[] temp = new int[high - low +1]; // Use a temporary array to store the span
+		int i = low;
+		int j = mid +1;
+		int k = 0;
 		
-		System.out.println("mid="+mid);
-		
-		while (prior <= mid && posterior <= high) {
-			if (arr[prior] <= arr[posterior]) {
-				// Save the smaller one to buffer
-				buffer[k] = arr[prior];
-				prior++;
-				k++;
+		while (i <= mid && j <= high) {		// The comparison part is the sort of the Quick-Sort, same as it
+			if (nums[i] < nums[j]) {
+				temp[k++] = nums[i++];
 			} else {
-				buffer[k] = arr[posterior];
-				posterior++;
-				k++;
+				temp[k++] = nums[j++];
 			}
 		}
 		
-		if (prior <= mid) {		// The first half has rest elements
-			for (int i = prior; i <= mid; i++) {
-				buffer[k] = arr[i];
-				k++;
-			}
+		while (i <= mid) {					// Append the rest elements of the left part to the temporary array
+			temp[k++] = nums[i++];
 		}
 		
-		if (posterior <= high) {		// The second half has rest elements
-			for (int i = posterior; i <= high; i++) {
-				buffer[k] = arr[i];
-				k++;
-			}
+		while (j <= high) {					// Append the rest elements of the right part to the temporary array
+			temp[k++] = nums[j++];
 		}
 		
-		// Copy buffer to the original array
-		int m = low;
-		for (int i = 0; i < buffer.length; i++) {
-			arr[m] = buffer[i];
-			m++;
+		// Replace the sorted span to the corresponding position in the original array
+		for (int k2 = 0; k2 < temp.length; k2++) {
+			nums[low+k2] = temp[k2];
 		}
 		
 	}
-	
-	
-	public void split(int gap, int arr[]){		// Split the whole array with different span and apply the merge-sort to each span
-		int span = gap*2;
-		for (int i = 0; i < arr.length; i+=span) {
-			int start = i;
-			int end = i+span-1;
-			if (end > arr.length-1) {
-				end = arr.length-1;
-			}
-			System.out.println("start:"+start+"   end:"+end);
-			
-			merge(arr, start, end);
-		}
-	}
-	
-	
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MergeSort ms = new MergeSort();
-		int arr[] = {9,1,5,3,4,2,6,8,7};
-		for (int i = 1; i < arr.length; i*=2) {
-			System.out.println("gap="+i);
-			ms.split(i, arr);
-		}
-		
-		
-		// Print all the sorted elements
-		for (int i : arr) {
-			System.out.println(i);
-		}
-		
+		int[] nums = { 2, 7, 8, 3, 1, 6, 9, 0, 5, 4, 12, 16, 10, 11, 14, 13, 15 ,2};  
+		  
+        MergeSort.sort(nums, 0, nums.length-1);  
+        System.out.println(Arrays.toString(nums));
 	}
 
 }
